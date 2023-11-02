0017a4161f26620e20ce87c777e3f279f8615409
Andy <shikesenpu@gmail.com>
2023-11-02 11:27:00 +0100

    Fixed routing

---
+++ deltadrivefe/src/pages/Login/Login.jsx
@@ -0,0 +1,18 @@
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as Yup from "yup";
+
+
+import React from 'react';
+import {LoginForm} from "../../components/User/LoginForm";
+
+const Login = () => {
+    return (
+        <div>
+            <h1>Login</h1>
+            <LoginForm></LoginForm>
+        </div>
+    );
+}
+
+export default Login;
---
+++ deltadrivefe/src/pages/Register/Register.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {RegisterForm} from "../../components/User/RegisterForm";
+
+const Register = () => {
+    return (
+        <div>
+            <h1>Register</h1>
+            <RegisterForm></RegisterForm>
+        </div>
+    );
+}
+
+export default Register;
